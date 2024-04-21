a92ae5ece061d2b57c2fa66a5158f47fd07d7e36
RedonMehmeti <redonmehmeti2019@gmail.com>
2024-04-21 08:37:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("HI")
+
+function sayHi(){
+    console.log("Hello")
+}
+sayHi();
+sayHi();
+
+function sum(a, b){
+    console.log(b)
+    console.log(a + b)
+}
+sum(1, 2);
+
+
+function greet(name, lastName){
+  console.log('Hello' + name + '' + lastName);
+}
+
+greet('Redon', 'Mehmeti');
+
+let x = myFunction(4,3);
+document.getElementById("jsf").innerHTML = x;
+
+function myFunction(a, b){
+    return a * b
+}
+
